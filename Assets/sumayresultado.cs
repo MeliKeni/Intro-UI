66e69484f85d0d3139b13b376a5c04dd9524c8bb
MeliKeni <melinakenigsberg@gmail.com>
2025-05-13 09:01:30 -0300

    ejercicio 4 listo

---
+++ Assets/sumayresultado.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class sumayresultado : MonoBehaviour
+{
+    public TMP_InputField InputNum1;
+    public TMP_InputField InputNum2;
+    public TextMeshProUGUI TxtResultado;
+    int num1;
+    int num2;
+    int resultado;
+
+    void Start()
+    {
+        TxtResultado.text = "";
+
+    }
+    public void SumarYMostrar()
+    {
+        num1 = int.Parse(InputNum1.text);
+        num2 = int.Parse(InputNum2.text);
+        resultado = num1 + num2;
+        TxtResultado.text = (resultado).ToString();
+    }
+}
